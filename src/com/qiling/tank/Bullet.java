e567153ac14527643d30b4508d735df748da39f8
wodezxzl <2534618940@qq.com>
2021-04-23 13:56:01 +0800

    将子弹换为图片

--- src/com/qiling/tank/Bullet.java
+++ src/com/qiling/tank/Bullet.java
@@ -4,8 +4,8 @@ import java.awt.*;
 
 public class Bullet {
     private static final int SPEED = 10;
-    private static final int WIDTH = 10;
-    private static final int HEIGHT = 10;
+    private static final int WIDTH = 12;
+    private static final int HEIGHT = 5;
     private int x, y;
     private final Dir dir;
     private boolean live = true;
@@ -22,10 +22,20 @@ public class Bullet {
         if (!live) {
             tankFrame.bullets.remove(this);
         }
-        Color c = g.getColor();
-        g.setColor(Color.RED);
-        g.fillOval(x, y, WIDTH, HEIGHT);
-        g.setColor(c);
+        switch (dir) {
+            case LEFT:
+                g.drawImage(ResourceMgr.bulletL, x, y, null);
+                break;
+            case UP:
+                g.drawImage(ResourceMgr.bulletU, x, y, null);
+                break;
+            case RIGHT:
+                g.drawImage(ResourceMgr.bulletR, x, y, null);
+                break;
+            case DOWN:
+                g.drawImage(ResourceMgr.bulletD, x, y, null);
+                break;
+        }
         move();
     }
 
--- src/com/qiling/tank/ResourceMgr.java
+++ src/com/qiling/tank/ResourceMgr.java
@@ -7,14 +7,19 @@ import java.util.Objects;
 
 public class ResourceMgr {
     public static BufferedImage tankL, tankU, tankR, tankD;
+    public static BufferedImage bulletL, bulletU, bulletR, bulletD;
 
     static {
-        // 初始化时把坦克图片全部加载到内存
+        // 初始化时把坦克和子弹图片全部加载到内存
         try {
             tankL = ImageIO.read(Objects.requireNonNull(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankL.gif")));
             tankU = ImageIO.read(Objects.requireNonNull(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankU.gif")));
             tankR = ImageIO.read(Objects.requireNonNull(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankR.gif")));
             tankD = ImageIO.read(Objects.requireNonNull(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankD.gif")));
+            bulletL = ImageIO.read(Objects.requireNonNull(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletL.gif")));
+            bulletU = ImageIO.read(Objects.requireNonNull(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletU.gif")));
+            bulletR = ImageIO.read(Objects.requireNonNull(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletR.gif")));
+            bulletD = ImageIO.read(Objects.requireNonNull(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletD.gif")));
         } catch (IOException e) {
             e.printStackTrace();
         }
